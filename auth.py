0fa48b5f4e696880459deb221bf1b961317e9a2b
huyanh995 <huyanh995@gmail.com>
2019-05-28 20:26:04 +0700

    Update new functions

--- auth.py
+++ auth.py
@@ -53,7 +53,7 @@ def check_prodcorr(alpha_id, sess):
             check_prodcorr_url = "https://api.worldquantvrc.com/alphas/" + \
                 str(alpha_id) + "/correlations/prod"
             response = sess.get(check_prodcorr_url, data="", headers=headers)
-            if ERRORS(sess, response):
+            if ERRORS(sess, response.content):
                 time.sleep(5)
             elif "prodCorrelation" in response.text:
                 print("Tried times: "+str(tried_times))
@@ -84,7 +84,7 @@ def check_selfcorr(alpha_id, sess):
             check_selfcorr_url = "https://api.worldquantvrc.com/alphas/" + \
                 alpha_id + "/correlations/self"
             response = sess.get(check_selfcorr_url, data="", headers=headers)
-            if ERRORS(sess, response):
+            if ERRORS(sess, response.content):
                 time.sleep(5)
             elif "selfCorrelation" in response.text:
                 print("Tried times: "+str(tried_times))
@@ -133,16 +133,53 @@ def get_myalpha(sess, region, top="", min_sharpe=0.7, min_fitness=0.7, offset=0)
     return json.loads(response.content)["results"]
     # For further use, get alpha id in ["id"] of each alpha in results
 
-def get_alpha(sess, page):
-    response = get_myalpha(sess, offset=page)
-    my_alpha_res_obj = json.loads(response.content)["results"]
-    for alpha in my_alpha_res_obj:
-        alpha_id = alpha['id']
-        submit(alpha_id, sess)
-    if "Incorrect authentication credentials" in response.text:
-        login(login_url, sess)
+def get_alpha_info(alpha_id, sess):
+    # Get the alpha infomation including performance and some test results.
+    # Using it with simulate to complete the step
+    # Note that the simulate function only return alpha ID, which have to use this fuction
+    # to receive important information
+    # Another purpose is to filter alphas for signals, alphas which satisfied sharpe and fitness
+    # greater or equal than a determined value and passed the weight test is considered to be signal 
+    alpha_url_info = alpha_url + str(alpha_id)
+    response = sess.get(alpha_url_info, data="", headers = headers)
+    alpha_res_json = json.loads(response.content)
+    try:
+        if ERRORS(sess, response.content):
+            time.sleep(3)
+        else:
+            alpha_info = {}
+            alpha_info["alpha_id"]=alpha_res_json["id"]
+            alpha_info["create_day"]=int(alpha_res_json["dateCreated"].split("T")[0].replace("-",""))
+            alpha_info["alpha_code"]=alpha_res_json["code"]
+            alpha_info["settings"]=alpha_res_json["settings"]
+            alpha_info["sharpe"]=alpha_res_json["is"]["sharpe"]
+            alpha_info["fitness"]=alpha_res_json["is"]["fitness"]
+            alpha_info["grade"]=alpha_res_json["grade"]
+            for test in alpha_res_json["is"]["checks"]:
+                if test["name"] == "CONCENTRATED_WEIGHT":
+                    print(test["result"])
+                    alpha_info["weight_test"]=test["result"]
+                    break
+                elif len(alpha_res_json["is"]["details"]["records"]) < 12:
+                    alpha_info["weight_test"]="FAIL"
+                    break
+            alpha_info["longCount"]=alpha_res_json["is"]["longCount"]
+            alpha_info["shortCount"]=alpha_res_json["is"]["shortCount"]
+            alpha_info["pnl"]=alpha_res_json["is"]["pnl"]
+            alpha_info["turnover"]=alpha_res_json["is"]["turnover"]
+            alpha_info["margin"]=alpha_res_json["is"]["margin"]
+            alpha_info["drawdown"]=alpha_res_json["is"]["drawdown"]
+            alpha_info["status"]=alpha_res_json["status"]
+            return alpha_info
+    except ConnectionError:
+        print('CONNECTION LOST!')
+    except:
+        print('EXCEPTION {}'.format(response.content))
+        for x in alpha_info:
+            alpha_info[x] = ""
+        return alpha_info
 
-def check_submission(alpha_id, sess):
+def check_submission(alpha_id, sess): # Need to check again, exception at b' ' << Check later. THROTTLED << Check its meaning.
     # Check submission of an alpha:
     # Criteria: There're about 8 (+1 UNIT TEST) tests. The first five tests (excluding Self/Prod Corr and IS Sharpe Ladder)
     # are tested. If any of them is failed (STATUS: FAIL), then the last three tests are cancelled (STATUS: PENDING)
@@ -160,11 +197,11 @@ def check_submission(alpha_id, sess):
             check_submision_url = "https://api.worldquantvrc.com/alphas/" + \
                 str(alpha_id) + "/check"
             response = sess.get(check_submision_url, data="", headers=headers)
-            print(response.text)
+            #print(response.text)
             if 'FAIL' in response.text:
-                print("FAIL") # For testing only
-                return False, -1, -1
-            elif 'PENDING' in response.text or ERRORS(sess, response):
+                #print("FAIL") # For testing only
+                return False, -1, -1, tried_times
+            elif 'PENDING' in response.text or ERRORS(sess, response.content):
                 time.sleep(3)
             else:
                 list_test = json.loads(response.content)["is"]["checks"]
@@ -173,13 +210,16 @@ def check_submission(alpha_id, sess):
                         self_corr = check['value']
                     if check['name'] == 'PROD_CORRELATION':
                         prod_corr = check['value'] 
-                return True, self_corr, prod_corr
+                return True, self_corr, prod_corr, tried_times
             tried_times = tried_times + 1
         except ConnectionError:
             time.sleep(20)
         except:
-            return True, -1, -1
-    return True, -1, -1
+            f = open("log.txt","a+")
+            f.write('EXCEPTION: {}' .format(response.content))
+            f.close()
+            return True, -1, -1, tried_times
+    return True, -1, -1, tried_times
 
 def hide_alpha(alpha_id, sess):
     # Hide alphas those are not qualified tested or not good enough to use
@@ -194,7 +234,7 @@ def hide_alpha(alpha_id, sess):
     except:
         print("THERE IS AN EXCEPTION HERE, HELPPP!")
 
-def simulate_alpha(sess, alpha_code, top, region):
+def simulate_alpha(sess, alpha_code, top, region): # Need to check again.
     # Simulate alpha (mostly use for signals). Input alpha, universe and region.
     # For signals simulation, alphas have fitness > 0.7, sharpe > 0.7 and corr < determined values are called signals.
     max_tried_times = 10
@@ -244,15 +284,18 @@ def change_name(alpha_id, sess, name="anonymous"):
 sess = requests.session()
 login(login_url, sess)
 
-check_submission("Qmv2kVK",sess)
-print("=====")
-for x in range(101):
-    for y in get_myalpha(sess, region = "EUR", min_sharpe=1.25, min_fitness=2, offset=x):
-        alpha_id = y["id"]
-        can_submit, self_corr, prod_corr = check_submission(alpha_id,sess)
-        if can_submit == True:
-            if self_corr < 0.7 and prod_corr < 0.7:
-                 change_name(alpha_id, sess, name = "flag")
+alpha_id = "JRW0Kvn"
+for x, y in get_alpha_info("LO78Gv6",sess).items():
+    print(str(x)+" : "+str(type(y)))
+# for x in range(101):
+#     for y in get_myalpha(sess, region = "EUR", min_sharpe=1.25, min_fitness=2, offset=x):
+#         alpha_id = y["id"]
+#         can_submit, self_corr, prod_corr, tried_times = check_submission(alpha_id,sess)
+#         print(str(can_submit)+" : "+str(self_corr)+" : "+str(prod_corr)+" : "+str(tried_times))
+#         time.sleep(20)
+#         if can_submit == True:
+#             if self_corr < 0.7 and prod_corr < 0.7:
+#                  change_name(alpha_id, sess, name = "flag")
             
 
 
