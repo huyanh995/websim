b83a29ae48987a3f3adb2a53736dc7737549d516
huyanh995 <huyanh995@gmail.com>
2019-05-30 17:19:49 +0700

    Update

---
+++ submit.py
@@ -0,0 +1,10 @@
+import requests
+import json
+import time
+import threading
+from common import config
+import mysql.connector as mysql
+
+from datetime import datetime
+
+# Submit file. It is not the necessary part of the project. But I wrote it when my code is not ready to use (in beta test)
