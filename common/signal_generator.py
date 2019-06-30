7c915c88d57fb74ea779e05569321c9a3f515b3e
huyanh995 <huyanh995@gmail.com>
2019-07-01 02:34:55 +0700

    Fix bugs

--- common/signal_generator.py
+++ common/signal_generator.py
@@ -27,16 +27,24 @@ def get_combo_data(num,data):
             if num_ope > 1:
                 filled_data = "add("
                 for ope in rndOpe:
-                    filled_data = filled_data + ope[0].format(*random.sample(data,ope[1])) + ", "
+                    if ope[2] == 0:
+                        filled_data = filled_data + ope[0].format(*random.sample(data,ope[1])) + ", "
+                    else:
+                        rndGroup = random.choice(group)
+                        filled_data = filled_data + ope[0].format(*random.sample(data,ope[1]), rndGroup) + ", "
                 filter = random.choice(['true', 'false'])
                 filled_data = filled_data + "filter = " + filter + ")"
                 rndData.append(filled_data)
             else:
-                rndData.append(rndOpe[0][0].format(*random.sample(data,rndOpe[0][1])))
+                if rndOpe[0][2] == 0:
+                    rndData.append(rndOpe[0][0].format(*random.sample(data,rndOpe[0][1])))
+                else:
+                    rndGroup = random.choice(group)
+                    rndData.append(rndOpe[0][0].format(*random.sample(data,rndOpe[0][1]), rndGroup))
         return rndData
     except Exception as ex:
         trace_msg = traceback.format_exception(etype=type(ex), value=ex, tb=ex.__traceback__)
-        utils.db_insert_log("get_combo_data", str(trace_msg), str(rndData))
+        utils.db_insert_log("get_combo_data", str(trace_msg), str(rndData)+str(rndOpe))
 
 
 def get_alphas(data):
@@ -55,6 +63,3 @@ def get_alphas(data):
     except Exception as ex:
         trace_msg = traceback.format_exception(etype=type(ex), value=ex, tb=ex.__traceback__)
         utils.db_insert_log("get_alphas", str(trace_msg), str(alphas))
-
-
-
