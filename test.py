6e3e6c63b7f797875fa9f5a0678a4cc34aee08db
huyanh995 <huyanh995@gmail.com>
2019-08-16 15:36:50 +0700

    Update test.py

--- test.py
+++ test.py
@@ -17,9 +17,14 @@ utils.login(sess)
 select_query = "SELECT alpha_code, universe, region FROM signals"
 db = mysql.connect(**config.config_db)
 cursor = db.cursor()
+cursor.execute("SELECT count(*) FROM signals")
+num_alpha = cursor.fetchall()[0]
 cursor.execute(select_query)
 results = cursor.fetchall()
+count = 0
 for result in results:
+    count = count + 1
+    print("{}/{}".format(count, num_alpha))
     alpha_id = simulator.simulate_alpha(sess, result[0], result[1], result[2], 1)
     if alpha_id != None:
         print(utils.get_alpha_info(alpha_id, sess))
